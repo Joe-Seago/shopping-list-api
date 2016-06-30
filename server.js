4a56135f5be730096ac9b269a8fbab9a2097cd86
jbenjaminy <young.benjamin@gmail.com>
2016-06-30 16:26:10 -0500

    Adds delete user request

--- server.js
+++ server.js
@@ -9,28 +9,34 @@ var jsonParser = bodyParser.json();
 /*---------- CONSTRUCTORS -----------*/
 
 var Storage = function() {
-  this.items = [];
-  this.id = 0;
-  this.users = [];
+    this.items = [];
+    this.id = 0;
+    this.users = [];
 };
 
 Storage.prototype.add = function(name) {
-  var item = { name: name, id: this.id };
-  this.items.push(item);
-  this.id += 1;
-  return item;
+    var item = {
+        name: name,
+        id: this.id
+    };
+    this.items.push(item);
+    this.id += 1;
+    return item;
 };
 
 Storage.prototype.addByID = function(name, id) {
-  var item = { name: name, id: id };
-  this.items.push(item);
-  return item;
+    var item = {
+        name: name,
+        id: id
+    };
+    this.items.push(item);
+    return item;
 };
 
 Storage.prototype.addUser = function(body) {
-  this.users.push(body);
-  this.items = this.items.concat(body.items);
-  return body;
+    this.users.push(body);
+    this.items = this.items.concat(body.items);
+    return body;
 };
 
 
@@ -41,71 +47,82 @@ storage.add('Peppers');
 
 /*---------- GET REQUESTS ----------*/
 app.get('/items', function(request, response) {
-  response.json(storage.items);
+    response.json(storage.items);
 });
 
 app.get('/items/:id', function(request, response) {
-  response.json(storage.items[request.params.id]);
+    response.json(storage.items[request.params.id]);
 });
 
 app.get('/users', function(request, response) {
-  response.json(storage.users);
+    response.json(storage.users);
 });
 
 app.get('/users/:username/items', function(request, response) {
-  for (var i = 0; i < storage.users.length; i++) {
-    if (request.params.username === storage.users[i].username) {
-      return response.json(storage.users[i].items);
+    for (var i = 0; i < storage.users.length; i++) {
+        if (request.params.username === storage.users[i].username) {
+            return response.json(storage.users[i].items);
+        }
     }
-  }
-  response.sendStatus(404);
+    response.sendStatus(404);
 });
 
 /*---------- POST REQUESTS ----------*/
 app.post('/items', jsonParser, function(request, response) {
-  if (!request.body) {
-    return response.sendStatus(400);
-  }
+    if (!request.body) {
+        return response.sendStatus(400);
+    }
 
-  var item = storage.add(request.body.name);
-  response.status(201).json(item);
+    var item = storage.add(request.body.name);
+    response.status(201).json(item);
 });
 
 app.post('/users', jsonParser, function(request, response) {
-  if (!request.body) {
-    return response.sendStatus(400);
-  }
+    if (!request.body) {
+        return response.sendStatus(400);
+    }
 
-  var body = storage.addUser(request.body);
-  response.status(201).json(body);
+    var body = storage.addUser(request.body);
+    response.status(201).json(body);
 });
 
 /*---------- DELETE REQUEST ----------*/
 app.delete('/items/:id', function(request, response) {
-  var id = request.params.id;
+    var id = request.params.id;
 
-  if (!storage.items[id]) {
-    return response.sendStatus(404);
-  }
+    if (!storage.items[id]) {
+        return response.sendStatus(404);
+    }
 
-  var item = storage.items[id];
-  storage.items.splice(id, 1);
-  response.status(200).json(item);
+    var item = storage.items[id];
+    storage.items.splice(id, 1);
+    response.status(200).json(item);
+});
+
+app.delete('/users/:username', function(request, response) {
+    for (var i = 0; i < storage.users.length; i++) {
+        if (request.params.username === storage.users[i].username) {
+            var user = storage.users[i];
+            storage.users.splice(i, 1);
+            return response.status(200).json(user);
+        }
+    }
+    response.sendStatus(404);
 });
 
 /*---------- PUT REQUEST ----------*/
 app.put('/items/:id', jsonParser, function(request, response) {
-  var putID = parseInt(request.params.id);
-  var putName = request.body.name;
+    var putID = parseInt(request.params.id);
+    var putName = request.body.name;
 
-  for (var i = 0; i < storage.items.length; i++) {
-    if (storage.items[i].id === putID) {
-      storage.items[i].name = request.body.name;
-      return response.status(200).json(storage.items[i]);
+    for (var i = 0; i < storage.items.length; i++) {
+        if (storage.items[i].id === putID) {
+            storage.items[i].name = request.body.name;
+            return response.status(200).json(storage.items[i]);
+        }
     }
-  }
-  var item = storage.addByID(putName, putID);
-  response.status(201).json(item);
+    var item = storage.addByID(putName, putID);
+    response.status(201).json(item);
 });
 
 app.listen(process.env.PORT || 8080);
