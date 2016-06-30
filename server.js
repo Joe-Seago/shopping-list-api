877045df9191a3031500f0813630a2797c1416a4
jbenjaminy <young.benjamin@gmail.com>
2016-06-30 11:53:03 -0500

    adds delete and put requests

--- server.js
+++ server.js
@@ -24,16 +24,41 @@ var jsonParser = bodyParser.json();
 var app = express();
 app.use(express.static('public'));
 
-app.get('/items', function(req, res) {
-    res.json(storage.items);
+app.get('/items', function(request, response) {
+    response.json(storage.items);
 });
 
-app.post('/items', jsonParser, function(req, res) {
-	if (!req.body) {
-		return res.sendStatus(400);
+app.post('/items', jsonParser, function(request, response) {
+	if (!request.body) {
+		return response.sendStatus(400);
 	}
-	var item = storage.add(req.body.name);
-	res.status(201).json(item);
+	var item = storage.add(request.body.name);
+	response.status(201).json(item);
+});
+
+app.delete('/items/:id', function(request, response) {
+	var id = request.params.id
+	
+	if (!storage.items[id]) {
+		return response.sendStatus(404);
+	}
+
+	var item = storage.items[id];
+	storage.items.splice(id, 1);
+	response.status(200).json(item);
+});
+
+app.put('/items/:id', jsonParser, function(request, response) {
+	var id = request.params.id;
+	var name = request.body.name;
+		// console.log(id, "<--id");
+		// console.log(name, "<--name");
+		// console.log(!storage.items[id], "<--truthy?");
+		// console.log(request.body, "<---request");
+	if (!storage.items[id]) {
+		storage.add(name); 
+		response.status(201).json(name);
+	}
 });
 
 app.listen(process.env.PORT || 8080);
